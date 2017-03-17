7654cd8b371595c70f9e81b7bb33a9ec1b56b281
CodeRider <coderiderpl@gmail.com>
2017-03-17 21:40:36 +0100

    Add files via upload

---
+++ example/basic.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeRider\Captcher;
+
+$captcha = new Generator(250, 50);
+$captchaImage = $captcha->generate();
+$captchaText = $captcha->getCaptchaText();
+
+
+echo "<img src='data:image/jpeg;base64," . base64_encode($captchaImage) . "'>";
+
+$_SESSION['captcha-protection'] = $captchaText;
+echo $captchaText;
